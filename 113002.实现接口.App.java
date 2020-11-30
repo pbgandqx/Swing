ef1ae6ae89d9bc1786daf4a3437571b26d1ebf65
pbgandqx <48664359+pbgandqx@users.noreply.github.com>
2020-11-30 15:40:04 +0800

    Create 113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,20 @@
+package pac02;
+
+public class App implements R {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}

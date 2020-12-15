f82ea1a0d10d6c6735a4c628c254992da51c264d
pbgandqx <48664359+pbgandqx@users.noreply.github.com>
2020-12-15 20:05:32 +0800

    Create 121402.斐波那契数列第n项.App.java

---
+++ 121402.斐波那契数列第n项.App.java
@@ -0,0 +1,27 @@
+package pacc;
+import java.util.Scanner;
+public class App {
+	
+	
+	    public static void main(String[] args) {
+	        int a=1,b=1,c=0;
+	        Scanner scanner = new Scanner(System.in );
+	        System.out.println("输入一个数字：");
+
+	        int user = scanner.nextInt();
+	        for (int i = 3; i < user; i++) {
+	            c=a+b;
+	            a=b;
+	            b=c;
+	        }
+	        if (user>0&&user<=3){
+	            if (user==1)System.out.println(0);
+	            if (user!=1)System.out.println(1);
+	        }else{
+	            System.out.println(c);
+	        }
+
+	    }
+	
+
+}
